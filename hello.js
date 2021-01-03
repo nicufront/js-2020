b7938e535009db38372d78b6200029c3487a947a
nicufront <74207361+nicufront@users.noreply.github.com>
2021-01-03 19:46:22 +0200

    curs 1

--- hello.js
+++ hello.js
@@ -1,6 +1,24 @@
-setTimeout(welcome, 3000)
+var time = 3000
+time = time + 1000
+var message = "Bine ai venit!"
+var isLate = false
+
+if (isLate) {
+    alert("Go home")
+}
+
+if (isLate) {
+    alert("Go Home")
+    
+} else {
+    alert("Continue")
+    
+}
+
+setTimeout(welcome, time)
+
 function welcome(){
-    alert("Bine ai venit!")
+    alert(message)
     alert("Welcome!")
 }
 
