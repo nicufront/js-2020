df33b30b98a08fa3d08e35da3f4b26403a216de4
nicufront <74207361+nicufront@users.noreply.github.com>
2021-01-12 19:07:24 +0200

    curs 4

--- car/car.js
+++ car/car.js
@@ -5,22 +5,46 @@ let dacia = {
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
+    consumption: 11.5,
+    batteryCapacity: 150,
+    batteryCharge: 150,
     paint: function(color) {
         this.color = color
+    },
+    move(distance) {
+        consumption = this.averageConsumtion * distance / 100
+        if (consumption > this.batteryCharge) {
+            this.batteryCharge = 0
+            this.mileage += this.distanceToEmpty()
+        } else {
+            this.batteryCharge -= consumption
+            this.mileage += distance
+        }
+    },
+    
+    recharge() {
+        this.batteryCharge = this.batteryCapacity
+    },
+    
+    distanceToEmpty() {
+        return 100 * this.batteryCharge / this.averageConsumption / this.averageConsumption
     }
+   
 }
+  
 let ferrari = {
     make: "Ferrari",
     model: "Dino",
-    color: "vrosu",
+    color: "rosu",
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
-    paint: function(color) {
+    paint: function (color) {
         this.color = color
     }
 }
 dacia.paint("galben")
+dacia.move(100)
 console.log(dacia)
 console.log(ferrari)
 
---
+++ plantes/planets.js
@@ -0,0 +1,22 @@
+let button = document.getElementById("check")
+button.addEventListener("click", function () {
+    let planet = document.getElementById("redplanet")
+
+    planet.innerText = "Alien invasion"
+    planet.classList.remove("alert")
+    //planet.style.color = "red"
+    //planet.style.fontWeight = "bold"
+    //planet.remove()
+})
+
+document.getElementById("reset")
+    .addEventListener("click", function () {
+        let planet = document.getElementById("redplanet")
+        planet.innerText = "Nothing to report"
+        planet.classList.remove("alert")
+    })
+
+document.getElementById("greenplanet")
+    .addEventListener("click", function () {
+        document.getElementById("greenplanet").classList.add("alert")    
+    })
