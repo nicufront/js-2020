2cb719add13e19fbb57e153203814c6fbbdede17
nicufront <74207361+nicufront@users.noreply.github.com>
2021-01-17 15:03:06 +0200

    curs 5

---
+++ array/array.js
@@ -0,0 +1,13 @@
+let bubbles = [60, 59, 48, 71, 45, 51, 65, 70, 49, 38]
+
+let fruits = ["ananas", "mango", "orange", "tomato", "cucumber"]
+
+bubbles.sort()
+max = bubbles.pop()
+
+
+fruits.sort()
+fruits.reverse()
+
+console.log(max)
+console.log(fruits)
